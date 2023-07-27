55bc2add6210cb057fa8b51ee481e166338bc8d1
Jorge <j-anaya-@hotmail.com>
2023-07-27 13:48:22 -0500

    storing the user object in the req object

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => console.log(error));
+});
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
