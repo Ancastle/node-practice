2d9500e449d1b6015ec7d21bb2718a738a720be5
Jorge <j-anaya-@hotmail.com>
2023-09-08 15:14:39 -0500

    improving the mongoDb database connection

--- utils/database.js
+++ utils/database.js
@@ -2,15 +2,30 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
+let _db; // holds the connection to the database
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://janaya0625:sAbmihQIpJxQDbrK@cluster0.rzdlri4.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://janaya0625:sAbmihQIpJxQDbrK@cluster0.rzdlri4.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("connected");
+      _db = client.db();
       callback(client);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.mongoConnect = mongoConnect;
+
+exports.getDb = getDb;
