5a6172b2df770b236bbe1fe65afeaa2ad02fdfce
Jorge <j-anaya-@hotmail.com>
2023-09-11 14:32:50 -0500

    creating cart and adding products

--- models/user.js
+++ models/user.js
@@ -2,14 +2,44 @@ const mongodb = require("mongodb");
 const getDb = require("../utils/database").getDb;
 
 class User {
-  constructor(username, email) {
-    this.username = username;
+  constructor(name, email, cart, id) {
+    this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
+
   save() {
     const db = getDb();
     db.collection("users")
       .insertOne(this)
+      .then()
+      .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    const db = getDb();
+
+    const cartProductIndex = this.cart.items.findIndex((item) => {
+      return item.productId.toString() === product._id.toString();
+    });
+
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex > -1) {
+      updatedCartItems[cartProductIndex].quantity += 1;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: 1,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    return db
+      .collection("users")
+      .updateOne({ _id: this._id }, { $set: { cart: updatedCart } })
       .then()
       .catch((err) => console.log(err));
   }
