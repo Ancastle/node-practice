0c2922bf6bc2ea4964838f0185c55bcd2fbf123a
Jorge <j-anaya-@hotmail.com>
2023-10-25 15:31:00 -0500

    validating password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 const router = express.Router();
 
 const authController = require("../controllers/auth");
@@ -14,16 +14,24 @@ router.get("/signup", authController.getSignup);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email")
-    .custom((value, { req }) => {
-      if (value === "j-anaya-@hotmail.com") {
-        throw new Error("This email address is forbidden.");
-      }
-      return true; // important to return true if your custom validation
-      // succeed so the flow can continue
-    }),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "j-anaya-@hotmail.com") {
+          throw new Error("This email address is forbidden.");
+        }
+        return true; // important to return true if your custom validation
+        // succeed so the flow can continue
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 
