6db2d8c0b318df5db1f8155993a39e8a2750321c
Jorge <j-anaya-@hotmail.com>
2023-11-03 17:15:11 -0500

    basic pagination and preparing data for advanced pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -22,12 +24,27 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getHome = (req, res, next) => {
+  const page = req.query.page;
+  let totalItems;
   Product.find()
+    .count()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/home", {
         pageTitle: "Welcome Home",
         active: "/",
         products: products,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
