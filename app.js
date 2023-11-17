64990dd9dc070175bac5a24ba6cef29d88d836f4
Jorge <j-anaya-@hotmail.com>
2023-11-17 18:01:04 -0500

    reverting the https config to deploy

--- app.js
+++ app.js
@@ -126,9 +126,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    https
-      .createServer({ key: privateKey, cert: certificate }, app)
-      .listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     const error = new Error(err);
